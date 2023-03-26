f43381a9c152062ba6ba604dac143e301bc1e7c9
Wang Tai <i@wangtai.me>
2023-03-26 20:41:20 +0800

    update main

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ import time
 import click
 import requests
 
-logging.basicConfig(level=logging.DEBUG)
-# logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
 class CONFIG:
-    base_url = "http://127.0.0.1:8081"
+    # base_url = "http://127.0.0.1:8081"
+    base_url = "https://hz-4.matpool.com:27680"
 
 
 @click.group()
@@ -27,8 +28,11 @@ def cli(config: str):
 
 @cli.command()
 @click.option("--path", default=".", help="Path to the project.")
-def deploy(path: str):
+@click.option("--job", default="", help="Name of the job.")
+def deploy(path: str, job: str):
     """Deploy a model to WeCloud"""
+    if not job:
+        job = click.prompt("Please enter a name of the job")
     click.echo("Packaging project...")
     tmp_file_name = "/tmp/wecloud-{}-{}.tar.gz".format(getpass.getuser(), math.floor(time.time()))
     with tarfile.open(tmp_file_name, "w:gz") as tar:
@@ -38,12 +42,21 @@ def deploy(path: str):
 
     click.echo("Deploying model to WeCloud...")
     resp = requests.post(CONFIG.base_url + "/cli/deploy",
+                         data={'job_name': job},
                          files={"file": open(tmp_file_name, "rb")},
                          stream=True)
     log.debug(resp.text)
     if resp.status_code == 200:
         os.remove(tmp_file_name)
-        click.echo("Deploying model to WeCloud successfully")
+        resp_json = resp.json()
+        if resp_json["status"]:
+            click.echo("Deploying model to WeCloud successfully")
+            click.echo(
+                f"""Please visit the following address: "http://101.42.238.229/#/create-deployment". Once there, select the task "{resp_json['data']['orch_id']}" labeled as "profiling".""")
+        else:
+            click.echo(f"Deploying model to WeCloud failed, {resp_json['message']}")
+    else:
+        click.echo(f"Deploying model to WeCloud failed,{resp.status_code}, {resp.text}")
 
 
 if __name__ == "__main__":
