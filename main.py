813de620ad0f25df111501b97547984e29b92039
Wang Tai <i@wangtai.me>
2023-04-03 19:06:10 +0800

    add async func

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import webbrowser
 
 import click
 import requests
+import yaml
+from yaml.parser import ParserError
 
 # logging.basicConfig(level=logging.DEBUG)
 logging.basicConfig(level=logging.INFO)
@@ -17,24 +19,70 @@ log = logging.getLogger(__name__)
 
 
 class CONFIG:
-    # base_url = "http://127.0.0.1:8081"
-    base_url = "https://hz-4.matpool.com:26263"
-    deploy_url = "http://101.42.238.229/#/create-deployment"
+    base_url = "http://127.0.0.1:8081"
+    # base_url = "https://hz-4.matpool.com:26263"
+    deploy_url = "http://101.42.238.229/create-deployment"
+    config_dir = os.path.join(os.path.expanduser("~"), ".spilot")
+
+
+def _get_token():
+    token = ""
+    try:
+        with open(os.path.join(CONFIG.config_dir, 'token'), "r") as f:
+            token = f.read()
+    except FileNotFoundError:
+        pass
+    return token
 
 
 @click.group()
 @click.option("--config", default="", help="Configuration file to use.")
 def cli(config: str):
     click.echo(f"Config: {config}")
+    os.makedirs(CONFIG.config_dir, exist_ok=True)
+
+
+@cli.command()
+def login():
+    """Login to Serverless Pilot"""
+    click.echo("Login to Serverless Pilot")
+    click.echo("Please enter your username and password")
+    username = click.prompt("Username")
+    password = click.prompt("Password", hide_input=True)
+    click.echo("Login...")
+    resp = requests.post(CONFIG.base_url + "/account/login",
+                         data={'username': username, 'password': password})
+    log.debug(resp.text)
+    if resp.status_code == 200:
+        resp_json = resp.json()
+        token = resp_json["access_token"]
+        # save token to ~/.spilot/token
+        with open(os.path.join(CONFIG.config_dir, 'token'), "w") as f:
+            f.write(token)
+        click.echo("Login successfully")
+    elif resp.status_code == 401:
+        click.echo("Username or password is incorrect")
+    else:
+        click.echo("Login failed")
 
 
 @cli.command()
 @click.option("--path", default=".", help="Path to the project.")
 @click.option("--job", default="", help="Name of the job.")
 def deploy(path: str, job: str):
-    """Deploy a model to WeCloud"""
+    """Deploy a model to Serverless Pilot"""
+    if not job:
+        with open(os.path.join(os.getcwd(), path, ".spilot.yaml"), "r") as f:
+            try:
+                meta_data_dict = yaml.safe_load(f)
+            except ParserError as e:
+                log.debug("ParserError: {}".format(e))
+                pass
+            job = meta_data_dict["job_name"]
     if not job:
         job = click.prompt("Please enter a name of the job")
+    log.debug("job: {}".format(job))
+
     click.echo("Packaging project...")
     tmp_file_name = "/tmp/wecloud-{}-{}.tar.gz".format(getpass.getuser(), math.floor(time.time()))
     with tarfile.open(tmp_file_name, "w:gz") as tar:
@@ -42,8 +90,11 @@ def deploy(path: str, job: str):
         tar.add(os.path.join(os.getcwd(), path), arcname="")
     click.echo("Packaging project successfully")
 
-    click.echo("Deploying model to WeCloud...")
+    click.echo("Deploying model to Serverless Pilot...")
     resp = requests.post(CONFIG.base_url + "/cli/deploy",
+                         headers={
+                             'Authorization': 'Bearer ' + _get_token(),
+                         },
                          data={'job_name': job},
                          files={"file": open(tmp_file_name, "rb")},
                          stream=True)
@@ -51,18 +102,37 @@ def deploy(path: str, job: str):
     if resp.status_code == 200:
         os.remove(tmp_file_name)
         resp_json = resp.json()
-        if resp_json["status"]:
-            click.echo("Deploying model to WeCloud successfully")
-            click.echo("----------------------------------------")
-            click.echo(
-                f"""Please visit the following address: "{CONFIG.deploy_url}". Once there, 
-select the task "{resp_json['data']['orch_id']}" labeled as "profiling".""")
-            webbrowser.open(f"{CONFIG.deploy_url}?orch_id={resp_json['data']['orch_id']}")
-        else:
-            click.echo(f"Deploying model to WeCloud failed, {resp_json['message']}")
-    else:
-        click.echo(f"Deploying model to WeCloud failed,{resp.status_code}, {resp.text}")
+        cli_id = resp_json["data"]["cli_id"]
+        log.debug("cli_id: {}".format(cli_id))
+        click.echo("----------------------------------------")
+        status = ""
+        while True:
+            status_resp = requests.get(f"{CONFIG.base_url}/cli/status/{cli_id}",
+                                       headers={
+                                           'Authorization': 'Bearer ' + _get_token(),
+                                       })
+            new_status = status_resp.json()["data"]["status"]
+            if status != new_status:
+                status = new_status
+                click.echo("Deploying model status: {}".format(status))
+            time.sleep(2)
+            if status == 'stopped':
+                break
+        click.echo("Deploying model to Serverless Pilot successfully")
+        click.echo("----------------------------------------")
+        orch_id_resp = requests.get(f"{CONFIG.base_url}/cli/{cli_id}/orch_id",
+                                    headers={
+                                        'Authorization': 'Bearer ' + _get_token(),
+                                    })
+        orch_id = orch_id_resp.json()["data"]["orch_id"]
+        click.echo(
+            f"""Please visit the following address: "{CONFIG.deploy_url}". Once there, 
+select the task "{orch_id}" labeled as "profiling".""")
+        webbrowser.open(f"{CONFIG.deploy_url}?orch_id={orch_id}")
 
+
+# else:
+# click.echo(f"Deploying model to Serverless Pilot failed,{resp.status_code}, {resp.text}")
 
 if __name__ == "__main__":
     cli()
