91fb17289bb27974d6c3a3b1c6ec75e836651f36
qipengwang <861026685@qq.com>
2023-04-20 16:46:27 +0800

    [feature] complete login

--- main.py
+++ main.py
@@ -33,19 +33,10 @@ def _get_token():
         with open(os.path.join(CONFIG.config_dir, 'token'), "r") as f:
             token = f.read()
     except FileNotFoundError:
-        pass
+        token = _login()
     return token
 
-
-@click.group()
-@click.option("--config", default="", help="Configuration file to use.")
-def cli(config: str):
-    click.echo(f"Config: {config}")
-    os.makedirs(CONFIG.config_dir, exist_ok=True)
-
-
-@cli.command()
-def login():
+def _login():
     """Login to Serverless Pilot"""
     click.echo("Login to Serverless Pilot")
     click.echo("Please enter your username and password")
@@ -62,35 +53,77 @@ def login():
         with open(os.path.join(CONFIG.config_dir, 'token'), "w") as f:
             f.write(token)
         click.echo("Login successfully")
+        return token
     elif resp.status_code == 401:
         click.echo("Username or password is incorrect")
     else:
         click.echo("Login failed")
+    return ""
 
+
+@click.group()
+@click.option("--config", default="", help="Configuration file to use.")
+def cli(config: str):
+    click.echo(f"Config: {config}")
+    os.makedirs(CONFIG.config_dir, exist_ok=True)
+
+
+@cli.command()
+def login():
+    _login()
 
 @cli.command()
 @click.option("--path", default=".", help="Path to the project.")
 @click.option("--job", default="", help="Name of the job.")
 def deploy(path: str, job: str):
     """Deploy a model to Serverless Pilot"""
+    spilot_fn = os.path.join(os.getcwd(), path, ".spilot.yaml")
+    _deploy_ts = math.floor(time.time())
     if not job:
-        with open(os.path.join(os.getcwd(), path, ".spilot.yaml"), "r") as f:
-            try:
+        try:
+            with open(spilot_fn) as f:
                 meta_data_dict = yaml.safe_load(f)
-            except ParserError as e:
-                log.debug("ParserError: {}".format(e))
-                pass
             job = meta_data_dict["job_name"]
+        except ParserError as e:
+            log.debug("ParserError: {}".format(e))
+        except FileNotFoundError as e:
+            log.debug("FileNotFoundError: {}".format(e))
     if not job:
         job = click.prompt("Please enter a name of the job")
     log.debug("job: {}".format(job))
 
+    # complete the .spilot.yaml
+    meta_data_dict = {
+        "job": job,
+        "image": "wangqipeng/wecloud_train:v0.2.0",
+        "setup": ["pip3 install -r requirements.txt"],
+    }
+    if not os.path.exists(spilot_fn):
+        meta_data_dict["run"] = click.prompt("Please enter the run command")
+    else:
+        try:
+            with open(spilot_fn) as f:
+                d = yaml.safe_load(f)
+            meta_data_dict["run"] = d["run"].strip()
+        except ParserError as e:
+            click.echo("yaml format is not correct, exit")
+            exit()
+    meta_data_dict["run"] = meta_data_dict["run"].split()
+    meta_data_dict["profile"] = meta_data_dict["run"].copy()
+    print("meta_data_dict", meta_data_dict)
+    saved_spilot_fn = "/tmp/.spilot-{}.yaml".format(_deploy_ts)
+    with open(saved_spilot_fn, "w") as f:
+        yaml.dump(meta_data_dict, f)
+    click.echo("save to {}".format(saved_spilot_fn))
+
     click.echo("Packaging project...")
-    tmp_file_name = "/tmp/wecloud-{}-{}.tar.gz".format(getpass.getuser(), math.floor(time.time()))
+    tmp_file_name = "/tmp/wecloud-{}-{}.tar.gz".format(getpass.getuser(), _deploy_ts)
     with tarfile.open(tmp_file_name, "w:gz") as tar:
         log.debug("path: {}".format(path))
+        tar.add(saved_spilot_fn, arcname=".spilot.yaml")
         tar.add(os.path.join(os.getcwd(), path), arcname="")
     click.echo("Packaging project successfully")
+    print(tmp_file_name)
 
     click.echo("----------------------------------------")
     click.echo("Deploying model to Serverless Pilot...")
@@ -109,6 +142,10 @@ def deploy(path: str, job: str):
         log.debug("cli_id: {}".format(cli_id))
         status = ""
         while True:
+            _token = _get_token()
+            if _token is None: 
+                click.echo("Login failed, exit. Please re-login and retry.")
+                exit()
             status_resp = requests.get(f"{CONFIG.base_url}/cli/status/{cli_id}",
                                        headers={
                                            'Authorization': 'Bearer ' + _get_token(),
@@ -128,10 +165,10 @@ def deploy(path: str, job: str):
                                     })
         log.debug(orch_id_resp.json())
         orch_id = orch_id_resp.json()["data"]["orch_id"]
-        click.echo(
-            f"""Please visit the following address: "{CONFIG.deploy_url}". Once there, 
-select the task "{orch_id}" labeled as "profiling".""")
+        click.echo(f'Please visit the following address: "{CONFIG.deploy_url}". Once there, select the task "{orch_id}" labeled as "profiling".')
         webbrowser.open(f"{CONFIG.deploy_url}?orch_id={orch_id}")
+    else:
+        click.echo("upload orch faild.")
 
 
 if __name__ == "__main__":
