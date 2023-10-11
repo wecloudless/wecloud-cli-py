3288f9e8d5a7b75dedf09c778b498f9be084e690
Rivendile <908636821@qq.com>
2023-10-11 20:18:01 +0800

    sync

--- main.py
+++ main.py
@@ -28,6 +28,11 @@ class CONFIG:
     base_url = "http://115.27.161.208/api/v1"
     deploy_url = "http://115.27.161.208/#/create-deployment"
     
+    
+    # linggui
+    # base_url = "http://192.168.20.175/api/v1"
+    # deploy_url = "http://192.168.20.175/#/create-deployment"
+    
     # xinjin
     # base_url = "http://162.105.175.222:8081"
     # deploy_url = "http://162.105.175.222:8080/#/create-deployment"
